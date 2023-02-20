716799d07dda81ca15189daf28e376e54913eb03
Ashley <ashleyd3391@gmail.com>
2023-02-19 20:18:55 -0600

    Added functions to create books

--- script.js
+++ script.js
@@ -2,22 +2,52 @@ const bookForm = document.getElementById("bookForm");
 const addBookButton = document.getElementById("addBook");
     addBookButton.addEventListener("click", addBookToLibrary);
 
-let myLibrary = [];
+let myLibrary = [{
 
-function Book() {
-    title: ""
-    pages: ""
-    author: ""
-    read: ""
-    //constructor
+    title: "Scapegracers",
+    author: "H.A. Clarke",
+    pages: "400",
+    read: "false"
+}, 
+{
+    title: "Morrigan's Cross",
+    author: "Nora Roberts",
+    pages: "400",
+    read: "true"
+}];
+
+function createBookElement(el, content, className){
+    const element = document.createElement(el);
+    element.textContent = content;
+    element.setAttribute("class", className);
+    return element;
+}
+
+function createBookItem(book, index) {
+    const bookItem = document.createElement("div");
+    bookItem.setAttribute("id", index);
+    bookItem.setAttribute("key", index);
+    bookItem.setAttribute("class", "card book");
+    bookItem.appendChild(createBookElement("h1", "Title: ${book.title}", "book-title"));
+    bookItem.appendChild(createBookElement("h1", "Author: ${book.author}", "book-author"));
+    bookItem.appendChild(createBookElement("h1", "Pages: ${book.pages}", "book-pages"));
+    book.insertAdjacentElement("afterbegin", bookItem);
 };
+
+function renderBooks(){
+    myLibrary.map((book, index) => {
+        createBookItem(book, index);
+    })
+};
+
+renderBooks();
 
 function addBookToLibrary() {
     document.getElementById("bookForm").style.display = "initial";
     bookForm.style.display = "grid";
     bookForm.style.gridTemplateColumns = "300px";
     bookForm.style.rowGap = "20px";
-    bookForm.style.backgroundColor = "pink";
+    bookForm.style.backgroundColor = "rgb(125, 125, 206)";
     bookForm.style.width = "300px";
     bookForm.style.padding = "20px";
     bookForm.style.marginLeft = "40%";
